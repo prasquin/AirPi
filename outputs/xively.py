e3bf2cf44e0367cf666ca0b0358e422057812b0e
Jncl <jncl@mac.com>
2014-04-14 21:57:25 +0100

    Extend error handling

--- outputs/xively.py
+++ outputs/xively.py
@@ -12,18 +12,18 @@ class Xively(output.Output):
         self.FeedID = data["FeedID"]
     def outputData(self, dataPoints):
         arr = []
-        for i in dataPoints:
-            # handle GPS data
-            if i["name"] == "Location":
-                arr.append({"location": {"disposition": i["Disposition"], "ele": i["Altitude"], "exposure": i["Exposure"],  "domain": "physical", "lat": i["Latitude"], "lon": i["Longitude"]}})
-            else:
-                arr.append({"id": i["name"], "current_value": i["value"]})
-        a = json.dumps({"version": "1.0.0", "datastreams": arr})
         try:
+            for i in dataPoints:
+                # handle GPS data
+                if i["name"] == "Location":
+                    arr.append({"location": {"disposition": i["Disposition"], "ele": i["Altitude"], "exposure": i["Exposure"], "domain": "physical", "lat": i["Latitude"], "lon": i["Longitude"]}})
+                else:
+                    arr.append({"id": i["name"], "current_value": i["value"]})
+            a = json.dumps({"version": "1.0.0", "datastreams": arr})
             z = requests.put("https://api.xively.com/v2/feeds/" + self.FeedID + ".json", headers = {"X-ApiKey":self.APIKey}, data = a)
             if z.text != "":
                 print "Xively Error: " + z.text
                 return False
+            return True
         except Exception as e:
             raise e
-        return True
